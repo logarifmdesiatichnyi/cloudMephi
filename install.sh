388e03c8a6c84c1091bd1d8ee50272510422ab61
Амгалан Владимирович <sharolyd@student.21-nsk.ru>
2024-10-20 20:21:37 +0300

    mark2

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -ex
 
-# Добавление репозитория PostgreSQL
+# Добавление репозитория PostgreSQL 15
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Импорт ключа для проверки пакетов PostgreSQL
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-# Обновление списка пакетов и установка PostgreSQL
+# Обновление списка пакетов
 sudo apt update
-sudo apt -y install postgresql
+
+# Установка PostgreSQL 15
+sudo apt -y install postgresql-15
 
 # Переход на пользователя postgres и проверка подключения к базе
 sudo -i -u postgres <<EOF
@@ -22,15 +24,19 @@ systemctl status postgresql
 
 # Добавление ключа и репозитория для pgAdmin
 wget --quiet -O - https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -
-
-# Добавление репозитория для pgAdmin4
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
 
 # Обновление списка пакетов и установка pgAdmin4
 sudo apt update
 sudo apt install -y pgadmin4
 
-# Изменение конфигурации pgAdmin для работы на 0.0.0.0 (доступ с любого IP)
+# Создание конфигурационного файла, если его нет
+if [ ! -f /etc/pgadmin4/config_local.py ]; then
+    sudo mkdir -p /etc/pgadmin4
+    echo "SERVER_MODE = False" | sudo tee /etc/pgadmin4/config_local.py
+fi
+
+# Настройка конфигурации pgAdmin для работы на 0.0.0.0
 sudo sed -i "s/127.0.0.1/0.0.0.0/" /etc/pgadmin4/config_local.py
 
 # Разрешение порта 80 в firewall
